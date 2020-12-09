7c3ba80345ad1375f458ce18f1535415c9299bb3
Yuri Brunetto <brunettoyuri@gmail.com>
2020-12-09 15:48:27 -0300

    add main seo

--- components/layout/index.js
+++ components/layout/index.js
@@ -12,6 +12,30 @@ function Layout({ children }) {
     <>
       <Head>
         <meta name='theme-color' content='#706FD3' />
+        <title>hello, i&apos;m yuri!</title>
+        <meta name='title' content='yuri brunetto' />
+        <meta
+          name='description'
+          content='yuri brunetto is a front-end developer, musician and lo-fi producer.'
+        />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://yuribrunetto.com.br/' />
+        <meta property='og:title' content='yuri brunetto' />
+        <meta
+          property='og:description'
+          content='yuri brunetto is a front-end developer, musician and lo-fi producer.'
+        />
+        <meta property='og:image' content='./logo512.png' />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://yuribrunetto.com.br/' />
+        <meta property='twitter:title' content='yuri brunetto' />
+        <meta
+          property='twitter:description'
+          content='yuri brunetto is a front-end developer, musician and lo-fi producer.'
+        />
+        <meta property='twitter:image' content='./twitter-card.png' />
       </Head>
 
       <MainStyled>
